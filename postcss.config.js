fac99156b06caca4a6c0b534eaa414cc2032bef4
frequentredundancy <freqred@hotmail.com>
2019-04-30 08:14:09 +0800

    initial

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   plugins: {
-    autoprefixer: {}
+    tailwindcss: './tailwind.config.js',
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production'
+      ? {
+          '@fullhuman/postcss-purgecss': {
+            content: ['./src/**/*.html', './src/**/*.vue', './src/**/*.js'],
+            defaultExtractor: content =>
+              content.match(/[A-Za-z0-9-_:/]+/g) || [],
+          },
+        }
+      : []),
   }
 }
